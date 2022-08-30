ee4d69d64a098b7e6529881e0feeab7da7d4b05b
grassfinn <theharper@live.com>
2022-08-30 17:33:26 -0400

    Intial Push

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './index.css'
+import data from './assets/data.js'
+import Card from './components/card.jsx'
+
+function App() {
+
+  const cards = data.map( item => {
+    return (
+      <Card
+        key= {item.id}
+        {...item}
+      />
+    )
+  })
+
+  return (
+    <div className="App">
+        {/* <Header/> */}
+        <section className='card-container'>
+          {cards}
+        </section>
+    </div>
+  )
+}
+
+export default App
+
+
+// create card template
+// filter through the cards and render each card based off the data of each object
---
+++ src/components/card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Card(props) {
+    console.log(props)
+
+    return (
+        <div className="card">
+            <img className="card-img" src="" alt={`Picture of ${props.title}`} />
+            <div className="card-info">
+                <span className="card-location"></span>
+                <span className="card-map"></span>
+                    <h2 className="card-title">{props.title}</h2>
+                <details open>
+                    <summary>Trip info</summary>
+                    <p className="card-duration"><b>{props.duration}</b></p>
+                    <p className="card-desc">{props.description}</p>
+                </details>
+            </div>
+        </div>
+    )
+}
+
+export default Card
